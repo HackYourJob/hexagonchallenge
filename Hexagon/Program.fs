669091653621df03580c2bb28f108d141c1e5b2b
fpellet <florent.pellet@gmail.com>
2016-12-16 15:58:57 +0100

    Initialize suave

--- Hexagon/Program.fs
+++ Hexagon/Program.fs
@@ -1,7 +1,31 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open Suave
+open Suave.Filters
+open Suave.Operators
+open System.IO
+
+let start port wwwDirectory =
+
+    let config = 
+        { defaultConfig with 
+                bindings = [ HttpBinding.mkSimple HTTP "0.0.0.0" port ]
+                homeFolder = Some (Path.GetFullPath wwwDirectory) }
+
+    let app : WebPart =
+      choose [
+        GET >=> path "/" >=> Files.browseFileHome "index.html"
+        GET >=> Files.browseHome
+        RequestErrors.NOT_FOUND "Page not found." 
+      ]
+
+    startWebServer config app
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+
+    start 8080 "../../../www"
+
     0 // return an integer exit code
