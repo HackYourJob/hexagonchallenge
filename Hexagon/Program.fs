a4560aae297c41ca31d2bae34f3b498168648234
fpellet <florent.pellet@gmail.com>
2016-12-16 16:33:36 +0100

    enable websocket

--- Hexagon/Program.fs
+++ Hexagon/Program.fs
@@ -1,10 +1,41 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-open Suave
-open Suave.Filters
-open Suave.Operators
+
+open System
 open System.IO
+open System.Net
+open System.Text
+
+open Suave
+open Suave.Http
+open Suave.Operators
+open Suave.Filters
+open Suave.Successful
+open Suave.Files
+open Suave.RequestErrors
+open Suave.Logging
+open Suave.Utils
+open Suave.Sockets
+open Suave.Sockets.Control
+open Suave.WebSocket
+
+let echo (webSocket : WebSocket) =
+  fun cx -> socket {
+    let loop = ref true
+    while !loop do
+      let! msg = webSocket.read()
+      match msg with
+      | (Text, data, true) ->
+        let str = Encoding.UTF8.GetString data
+        do! webSocket.send Text data true
+      | (Ping, _, _) ->
+        do! webSocket.send Pong [||] true
+      | (Close, _, _) ->
+        do! webSocket.send Close [||] true
+        loop := false
+      | _ -> ()
+  }
 
 let start port wwwDirectory =
 
@@ -15,6 +46,7 @@ let start port wwwDirectory =
 
     let app : WebPart =
       choose [
+        path "/websocket" >=> handShake echo
         GET >=> path "/" >=> Files.browseFileHome "index.html"
         GET >=> Files.browseHome
         RequestErrors.NOT_FOUND "Page not found." 
