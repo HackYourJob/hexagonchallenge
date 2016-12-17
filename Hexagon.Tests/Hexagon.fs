95ecd601b01aa882f4023024a1dcd7390c333fc2
fpellet <florent.pellet@gmail.com>
2016-12-17 12:35:33 +0100

    Generate hexagon

---
+++ Hexagon.Tests/Hexagon.fs
@@ -0,0 +1,74 @@
+﻿module Hexagon.Tests.Hexagon
+
+open System
+open Xunit
+open FsUnit.Xunit
+
+open Hexagon.Hexagon
+
+let display hexagon =
+    let serializeLine line =
+        line 
+        |> Seq.map (function Cell -> 'x' | None -> '.') 
+        |> String.Concat
+
+    hexagon 
+    |> Seq.map serializeLine 
+    |> Seq.toList
+
+type ``generateHexagon should`` ()=
+    [<Fact>] member x.
+     ``center cell on first line`` ()= 
+        generateHexagon 5 
+        |> display
+        |> Seq.head
+        |> should equal "....x...."
+
+    [<Fact>] member x.
+     ``two cells on second line`` ()= 
+        generateHexagon 5 
+        |> display
+        |> Seq.skip 1
+        |> Seq.head
+        |> should equal "...x.x..."
+
+    [<Fact>] member x.
+     ``header is arithmetic sequence of growth 2 with only pair value`` ()= 
+        generateHexagon 5 
+        |> display
+        |> Seq.take 5
+        |> Seq.toList
+        |> should equal 
+            ["....x...."
+             "...x.x..."
+             "..x.x.x.."
+             ".x.x.x.x."
+             "x.x.x.x.x"]
+
+    [<Fact>] member x.
+     ``body has hexagon size`` ()= 
+        generateHexagon 5 
+        |> display
+        |> Seq.skip 4
+        |> Seq.take 5
+        |> Seq.toList
+        |> should equal 
+            ["x.x.x.x.x"
+             "x.x.x.x.x"
+             "x.x.x.x.x"
+             "x.x.x.x.x"
+             "x.x.x.x.x"]
+
+    [<Fact>] member x.
+     ``footer is inverse of header`` ()= 
+        generateHexagon 5
+        |> display 
+        |> Seq.skip 4
+        |> Seq.skip 4
+        |> Seq.toList
+        |> should equal 
+            ["x.x.x.x.x"
+             ".x.x.x.x."
+             "..x.x.x.."
+             "...x.x..."
+             "....x...."]
---
+++ Hexagon/Hexagon.fs
@@ -0,0 +1,30 @@
+﻿module Hexagon.Hexagon
+
+type GeneratedCell = Cell | None
+
+let none _ = None
+let oddSeq i = if i%2 = 0 then None else Cell
+
+let generateLine size num =
+    seq {
+        yield! [1 .. size-num] |> Seq.map none
+        yield! [1 .. (1 + (num - 1) * 2)] |> Seq.map oddSeq
+        yield! [1 .. size-num] |> Seq.map none 
+    }
+
+let generateHeader size =
+    [1 .. size - 1] |> Seq.map (generateLine size)
+
+let generateFooter = generateHeader >> Seq.rev
+
+let generateBody size =
+    let fullLine _ = generateLine size size
+
+    [1 .. size] |> Seq.map fullLine
+
+let generateHexagon size = 
+    seq {
+        yield! generateHeader size
+        yield! generateBody size
+        yield! generateFooter size
+    }
