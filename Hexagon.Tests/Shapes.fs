f5011bf57aea9582cb0b8d1ff0c119b8edc57118
fpellet <florent.pellet@gmail.com>
2016-12-17 18:16:32 +0100

    Rename hexagon to shapes

---
+++ Hexagon.Tests/Shapes.fs
@@ -0,0 +1,77 @@
+﻿module Hexagon.Tests.Shapes
+
+open System
+open Xunit
+open FsUnit.Xunit
+
+open Hexagon.Shapes
+
+let display shape =
+    let serializeLine line =
+        line 
+        |> Seq.map (function Cell -> 'x' | None -> '.') 
+        |> String.Concat
+
+    shape 
+    |> Seq.map serializeLine 
+    |> Seq.toList
+
+module HexagonBoard =
+    open Hexagon.Shapes.HexagonBoard
+
+    type ``generateHexagon should`` ()=
+        [<Fact>] member x.
+         ``center cell on first line`` ()= 
+            generate 5 
+            |> display
+            |> Seq.head
+            |> should equal "....x...."
+
+        [<Fact>] member x.
+         ``two cells on second line`` ()= 
+            generate 5 
+            |> display
+            |> Seq.skip 1
+            |> Seq.head
+            |> should equal "...x.x..."
+
+        [<Fact>] member x.
+         ``header is arithmetic sequence of growth 2 with only pair value`` ()= 
+            generate 5 
+            |> display
+            |> Seq.take 5
+            |> Seq.toList
+            |> should equal 
+                ["....x...."
+                 "...x.x..."
+                 "..x.x.x.."
+                 ".x.x.x.x."
+                 "x.x.x.x.x"]
+
+        [<Fact>] member x.
+         ``body has hexagon size`` ()= 
+            generate 5 
+            |> display
+            |> Seq.skip 4
+            |> Seq.take 5
+            |> Seq.toList
+            |> should equal 
+                ["x.x.x.x.x"
+                 "x.x.x.x.x"
+                 "x.x.x.x.x"
+                 "x.x.x.x.x"
+                 "x.x.x.x.x"]
+
+        [<Fact>] member x.
+         ``footer is inverse of header`` ()= 
+            generate 5
+            |> display 
+            |> Seq.skip 4
+            |> Seq.skip 4
+            |> Seq.toList
+            |> should equal 
+                ["x.x.x.x.x"
+                 ".x.x.x.x."
+                 "..x.x.x.."
+                 "...x.x..."
+                 "....x...."]
---
+++ Hexagon/Shapes.fs
@@ -0,0 +1,31 @@
+﻿module Hexagon.Shapes
+
+type GeneratedCell = Cell | None
+
+let none _ = None
+let oddSeq i = if i%2 = 0 then None else Cell
+
+module HexagonBoard =
+    let private generateLine size num =
+        seq {
+            yield! [1 .. size-num] |> Seq.map none
+            yield! [1 .. (1 + (num - 1) * 2)] |> Seq.map oddSeq
+            yield! [1 .. size-num] |> Seq.map none 
+        }
+
+    let private generateHeader size =
+        [1 .. size - 1] |> Seq.map (generateLine size)
+
+    let private generateFooter = generateHeader >> Seq.rev
+
+    let private generateBody size =
+        let fullLine _ = generateLine size size
+
+        [1 .. size] |> Seq.map fullLine
+
+    let generate size = 
+        seq {
+            yield! generateHeader size
+            yield! generateBody size
+            yield! generateFooter size
+        }
