73ded313e3f326eee62db8ec84da52fba1f64a25
fpellet <florent.pellet@gmail.com>
2016-12-18 10:03:45 +0100

    fixup! convertShapeToCells

---
+++ Hexagon/Domain.fs
@@ -0,0 +1,22 @@
+﻿module Hexagon.Domain
+
+type AiId = int
+type AiDescription = { 
+        Id: AiId
+        Name: string
+    }
+
+type CellId = string
+type Cell = {
+        Id: CellId
+        LineNum: int
+        ColumnNum: int
+        State: CellState
+    }
+and CellState =
+    | Own of CellStateOwn
+    | Free of int
+and CellStateOwn = { AiId: AiId; Resources: int }
+
+type Board = Cell seq
+
