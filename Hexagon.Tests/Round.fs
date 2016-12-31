99ffd48bb278f2ed55cb5bb74c89f77ec40f99e9
fpellet <git@florentpellet.com>
2016-12-31 16:42:16 +0100

    valid transaction

---
+++ Hexagon.Tests/Round.fs
@@ -0,0 +1,81 @@
+﻿module Hexagon.Tests.Round
+
+open System
+open Xunit
+open FsUnit.Xunit
+
+open Hexagon.Domain
+
+module TransactionValidation =
+    open Hexagon.Round.TransactionValidation
+    
+    let aiId = 1
+
+    let cellOfAi = { Id = { LineNum = 1; ColumnNum = 1 }; State = Own { AiId = aiId; Resources = 5 }}
+    let freeCell = { Id = { LineNum = 1; ColumnNum = 2 }; State = Free 0}
+    let cellOfOtherAi = { Id = { LineNum = 1; ColumnNum = 3 }; State = Own { AiId = aiId + 2; Resources = 5 }}
+    let unknownCellId = { LineNum = 1; ColumnNum = 4 }
+
+    let neighboursCell = { Id = { LineNum = 2; ColumnNum = 2 }; State = Free 0}
+    let notNeighboursCell = { Id = { LineNum = 2; ColumnNum = 3 }; State = Free 0}
+
+    let getCell = function
+        | id when id = cellOfAi.Id -> Some cellOfAi
+        | id when id = freeCell.Id -> Some freeCell
+        | id when id = cellOfOtherAi.Id -> Some cellOfOtherAi
+        | id when id = neighboursCell.Id -> Some neighboursCell
+        | id when id = notNeighboursCell.Id -> Some notNeighboursCell
+        | _ -> None
+    let isNeighboursOf id1 id2 = id1 = cellOfAi.Id && id2 = neighboursCell.Id
+
+    let validAction = validAiAction getCell isNeighboursOf aiId
+
+    type ``validAiAction should`` ()=
+        [<Fact>] 
+        member x.``return same value if sleep`` ()= 
+            Sleep
+            |> validAction
+            |> should equal Sleep
+
+        [<Fact>] 
+        member x.``return same value if bug`` ()= 
+            Bug "Error"
+            |> validAction
+            |> should equal (Bug "Error")
+
+        [<Fact>] 
+        member x.``return bug if transaction not start of ai's cell`` ()= 
+            Transaction { FromId = cellOfOtherAi.Id; ToId = neighboursCell.Id; AmountToTransfert = 1 }
+            |> validAction
+            |> should equal (Bug "NotOwnCell")
+
+        [<Fact>] 
+        member x.``return bug if transaction start of free cell`` ()= 
+            Transaction { FromId = freeCell.Id; ToId = neighboursCell.Id; AmountToTransfert = 1 }
+            |> validAction
+            |> should equal (Bug "NotOwnCell")
+
+        [<Fact>] 
+        member x.``return bug if transaction move too resources`` ()= 
+            Transaction { FromId = cellOfAi.Id; ToId = neighboursCell.Id; AmountToTransfert = 6 }
+            |> validAction
+            |> should equal (Bug "NotEnoughResources")
+
+        [<Fact>] 
+        member x.``return bug if transaction not end of neighbours`` ()= 
+            Transaction { FromId = cellOfAi.Id; ToId = notNeighboursCell.Id; AmountToTransfert = 1 }
+            |> validAction
+            |> should equal (Bug "InvalidMove")
+
+        [<Fact>] 
+        member x.``return bug if transaction start of unknown cell`` ()= 
+            Transaction { FromId = unknownCellId; ToId = neighboursCell.Id; AmountToTransfert = 1 }
+            |> validAction
+            |> should equal (Bug "NotOwnCell")
+
+        [<Fact>] 
+        member x.``returntransaction if transaction is valid`` ()= 
+            let validTransaction = Transaction { FromId = cellOfAi.Id; ToId = neighboursCell.Id; AmountToTransfert = 5 }
+            validTransaction
+            |> validAction
+            |> should equal validTransaction
---
+++ Hexagon/Round.fs
@@ -0,0 +1,50 @@
+﻿module Hexagon.Round
+
+open Domain
+
+module TransactionValidation =
+    type private TransactionEither =
+        | Valid of TransactionEitherContext
+        | Invalid of InvalidReason
+    and TransactionEitherContext = { Transaction: TransactionParameters; FromCell: Cell; ToCell: Cell; AiId: AiId }
+    and InvalidReason =
+        | NotOwnCell
+        | NotEnoughResources
+        | InvalidMove
+
+    let private bind f = function | Invalid p -> Invalid p | Valid p -> f p
+
+    let private createContext (getCell: CellId -> Cell option) (aiId: AiId) (transaction: TransactionParameters) =
+        match getCell transaction.FromId, getCell transaction.ToId with
+        | Some fromCell, Some toCell -> Valid { FromCell = fromCell; ToCell = toCell; Transaction = transaction; AiId = aiId }
+        | _ -> Invalid NotOwnCell
+
+    let private validTransaction (isNeighboursOf: CellId -> CellId -> bool) (context: TransactionEither) =
+        let isOwnCell context =
+            match context.FromCell.State with
+            | Own param when param.AiId = context.AiId -> Valid context
+            | _ -> Invalid NotOwnCell
+
+        let haveEnoughResources context =
+            match extractResources context.FromCell with
+            | r when r >= context.Transaction.AmountToTransfert -> Valid context
+            | _ -> Invalid NotEnoughResources
+
+        let isNeighbours context =
+            if isNeighboursOf context.Transaction.FromId context.Transaction.ToId
+            then Valid context
+            else Invalid InvalidMove
+    
+        context
+        |> bind isOwnCell
+        |> bind haveEnoughResources
+        |> bind isNeighbours
+
+    let validAiAction getCell isNeighboursOf aiId (transaction: AiActions) =
+        match transaction with
+        | Sleep
+        | Bug _ -> transaction
+        | Transaction t -> 
+            match createContext getCell aiId t |> validTransaction isNeighboursOf with
+            | Invalid e -> sprintf "%A" e |> Bug
+            | Valid c -> transaction
