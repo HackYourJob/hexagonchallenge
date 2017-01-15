6d3a8ea35de2be315fbd5cc1ace6782c9c836485
fpellet <git@florentpellet.com>
2017-01-15 21:36:31 +0100

    Create store for ai cell ids

---
+++ Hexagon.Tests/AiCellIdsStore.fs
@@ -0,0 +1,45 @@
+﻿module Hexagon.Tests.AiCellIdsStore
+
+open System
+open Xunit
+open FsUnit.Xunit
+open Swensen.Unquote
+
+open Hexagon.Domain
+open Hexagon.AiCellIdsStore
+    
+let cellId1 = { LineNum = 1; ColumnNum = 1 }
+let cellId2 = { LineNum = 1; ColumnNum = 2 }
+let board = [
+        { Id = cellId1; State = Free 1 }
+        { Id = cellId2; State = Free 1 }
+        { Id = { LineNum = 2; ColumnNum = 1 }; State = Free 1 }
+        { Id = { LineNum = 2; ColumnNum = 2 }; State = Free 1 }
+    ]
+
+type ``Store should`` ()=
+    [<Fact>] 
+    member x.``always return same AiCellId for same CellId`` ()= 
+        let store = Store(board)
+
+        test <@ store.convertToAiCellId cellId1 = store.convertToAiCellId cellId1 @>
+
+    [<Fact>] 
+    member x.``generate unique id for each cells`` ()= 
+        let store = Store(board)
+
+        test <@ store.convertToAiCellId cellId1 <> store.convertToAiCellId cellId2 @>
+
+    [<Fact>] 
+    member x.``always get CellId of AiCellId`` ()= 
+        let store = Store(board)
+        let aiCellId = store.convertToAiCellId cellId1
+
+        test <@ store.convertToCellId aiCellId = cellId1 @>
+
+    [<Fact>] 
+    member x.``generate random ids`` ()= 
+        let store1 = Store(board)
+        let store2 = Store(board)
+
+        test <@ store1.convertToAiCellId cellId1 <> store2.convertToAiCellId cellId1 @>
---
+++ Hexagon/AiCellIdsStore.fs
@@ -0,0 +1,16 @@
+﻿module Hexagon.AiCellIdsStore
+
+open Domain
+
+let generateGuid _ = System.Guid.NewGuid().ToString()
+
+type Store (cells: Board)=
+    let cellIds = cells |> Seq.map (fun c -> c.Id, generateGuid()) |> dict
+    let aiCellIds = cellIds |> Seq.map (fun v -> v.Value, v.Key) |> dict
+
+    member x.convertToAiCellId id =
+        cellIds.[id]
+
+    member x.convertToCellId id =
+        aiCellIds.[id]
+
