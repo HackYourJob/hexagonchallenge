7a8f2d516ea692f925ecc43fd2103001a9e357da
fpellet <git@florentpellet.com>
2017-01-15 11:19:26 +0100

    Create very basic ai

---
+++ Hexagon/BasicAi.fs
@@ -0,0 +1,11 @@
+﻿module Hexagon.BasicAi
+
+open Ais
+
+let play (cells: AiCell list) : Transaction =
+    cells 
+    |> Seq.collect (fun c -> c.Neighbours |> Seq.map (fun n -> c, n))
+    |> Seq.filter (fun (_, n) -> n.Owner <> Own)
+    |> Seq.sortByDescending (fun (c, n) -> c.Resources - n.Resources)
+    |> Seq.head
+    |> (fun (c, n) -> Move { FromId = c.Id; ToId = n.Id; AmountToTransfert = c.Resources })
