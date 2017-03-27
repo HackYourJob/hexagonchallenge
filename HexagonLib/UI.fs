8c1b829e0eee6a3f63ce29e63a61bbf585882f86
Clément Bouillier <clement.bouillier@devcrafting.com>
2017-03-28 00:26:51 +0200

    Append legend update in F#/Fable

--- HexagonLib/UI.fs
+++ HexagonLib/UI.fs
@@ -10,7 +10,17 @@ type CellDom = {
     Cell: HTMLDivElement
 }
 
+type Score = {
+    CellsNb: int
+    CellsNbContainer: HTMLDivElement
+    ResourcesNb: int
+    ResourcesNbContainer: HTMLDivElement
+    BugsNb: int
+    BugsNbContainer: HTMLDivElement
+}
+
 let cellsDomByPosition = new System.Collections.Generic.Dictionary<string, CellDom>()
+let scoreByAi = new System.Collections.Generic.Dictionary<int, Score>()
 
 let createCell row col =
     let cell = document.createElement_div();
@@ -61,7 +71,7 @@ let changeOwner aiId resources cellId cell =
     cellsDomByPosition.[sprintf "%i-%i" cellId.LineNum cellId.ColumnNum] <- 
         { cell with AiId = aiId; Resources = resources }
 
-let updateResources resources cellId cell =
+let updateResources resources cellId (cell:CellDom) =
     changeOwner cell.AiId resources cellId cell
 
 let initializeCells cells =
@@ -73,10 +83,54 @@ let initializeCells cells =
         | Free n -> cell |> changeOwner 0 n c.Id
         | Own own -> cell |> changeOwner own.AiId own.Resources c.Id)
 
+let initializeLegend ais =
+    let scores = document.querySelector("#scores");
+    ais |> Seq.iter (fun (ai:AiDescription) -> 
+        let score = document.createElement_div();
+        score.className <- "score";
+        score.setAttribute("aiId", string ai.Id);
+        scores.appendChild(score) |> ignore;
+        
+        let legendContainer = document.createElement_div();
+        legendContainer.className <- "score-legend";
+        score.appendChild(legendContainer) |> ignore;
+        
+        let nameContainer = document.createElement_div();
+        nameContainer.className <- "score-aiName";
+        nameContainer.textContent <- ai.Name;
+        score.appendChild(nameContainer) |> ignore;
+        
+        let cellsNbContainer = document.createElement_div();
+        cellsNbContainer.className <- "score-cellsNb";
+        let cellsNb = 0;
+        cellsNbContainer.textContent <- string cellsNb;
+        score.appendChild(cellsNbContainer) |> ignore;
+        
+        let resourcesNbContainer = document.createElement_div();
+        resourcesNbContainer.className <- "score-resources";
+        let resourcesNb = 0;
+        resourcesNbContainer.textContent <- string resourcesNb;
+        score.appendChild(resourcesNbContainer) |> ignore;
+
+        let bugsNbContainer = document.createElement_div();
+        bugsNbContainer.className <- "score-bugs";
+        let bugsNb = 0;
+        bugsNbContainer.textContent <- string bugsNb;
+        score.appendChild(bugsNbContainer) |> ignore
+        
+        scoreByAi.[ai.Id] <- {
+            CellsNb = cellsNb
+            CellsNbContainer = cellsNbContainer
+            ResourcesNb = resourcesNb
+            ResourcesNbContainer = resourcesNbContainer
+            BugsNb = bugsNb
+            BugsNbContainer = bugsNbContainer
+        })
+
 let onStarted started =
     initializeBoard started.BoardSize
     initializeCells started.Board
-    //initializeLegend started.Ais
+    initializeLegend started.Ais
 
 let onCellOwned (cellOwned:CellOwned) =
     getCell cellOwned.CellId.LineNum cellOwned.CellId.ColumnNum
@@ -93,8 +147,26 @@ let onCellsChanged cellsChanged =
         | Owned cellOwned -> onCellOwned cellOwned
         | ResourcesChanged resourcesChanged -> onCellResourcesChanged resourcesChanged)
 
+let onTerritoryChanged (territoryChanged:TerritoryChanged) =
+    let previousScore = scoreByAi.[territoryChanged.AiId]
+    let score = { previousScore with 
+                    ResourcesNb = previousScore.ResourcesNb + territoryChanged.ResourcesIncrement; 
+                    CellsNb = previousScore.CellsNb + territoryChanged.CellsIncrement }
+    score.CellsNbContainer.textContent <- string score.CellsNb
+    score.ResourcesNbContainer.textContent <- string score.ResourcesNb
+    scoreByAi.[territoryChanged.AiId] <- score
+
+let onBugged aiId =
+    let previousScore = scoreByAi.[aiId]
+    let score = { previousScore with BugsNb = previousScore.BugsNb + 1 }
+    score.BugsNbContainer.textContent <- string score.BugsNb
+
 let onScoreChanged scoreChanged =
-    ()
+    scoreChanged
+    |> Seq.iter (fun e -> 
+        match e with
+        | TerritoryChanged territoryChanged -> onTerritoryChanged territoryChanged
+        | Bugged bugged -> onBugged bugged)
 
 let handleMessage = function
     | Started started -> onStarted started
@@ -102,3 +174,7 @@ let handleMessage = function
         onCellsChanged cellsChanged
         onScoreChanged scoreChanged
     | _ -> ()
+
+let optionSeqSeq = [[Some 2; Some 4]; [None; Some 3]]
+let collect = optionSeqSeq |> Seq.collect id
+printfn "collected nb = %i" (Seq.length collect)
