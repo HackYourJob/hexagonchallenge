8100c2cedce23ed1dec1fe7304b3be1177a6c84b
Clément Bouillier <clement.bouillier@devcrafting.com>
2017-04-11 21:22:35 +0200

    Draw games to play for a tounament : one game with additional AIs if needed

---
+++ Hexagon.Tests/CondorcetTournament.fs
@@ -0,0 +1,46 @@
+﻿module Hexagon.Tests.CondorcetTournament
+
+open Xunit
+open FsUnit.Xunit
+
+open Hexagon.CondorcetTournament
+open Hexagon.Domain
+open Hexagon.BasicAi
+
+type ``drawGames should`` () =
+    let sixPlayers = [ 
+        { Id = 1; Name = "BasicAi1" }, play 
+        { Id = 2; Name = "BasicAi2" }, play 
+        { Id = 3; Name = "BasicAi3" }, play 
+        { Id = 4; Name = "BasicAi4" }, play 
+        { Id = 5; Name = "BasicAi5" }, play 
+        { Id = 6; Name = "BasicAi6" }, play ]
+    
+    [<Fact>]
+    member x.``return no game when no AI`` ()=
+        drawTournament []
+        |> Seq.length
+        |> should equal 0
+        
+    [<Fact>]
+    member x.``return one game when 6 AI given randomly distributed`` ()=
+        let games = drawTournament sixPlayers
+        games |> should haveLength 1
+        
+        (games |> Seq.head).Players 
+        |> Seq.mapi (fun i p -> sixPlayers |> Seq.item i |> fst = fst p)
+        |> Seq.filter id
+        |> Seq.length
+        |> should lessThan 6
+                
+        (games |> Seq.head).Players
+        |> Seq.iter (fun p -> sixPlayers |> Seq.map fst |> should contain (fst p))
+
+    [<Fact>]
+    member x.``return game with additional AIs when strictly less than 6 AI given`` ()=
+        let firstGame = drawTournament (sixPlayers |> List.tail) |> Seq.head
+        firstGame.Players 
+        |> Seq.map (fun p -> (fst p).Id)
+        |> Seq.distinct 
+        |> Seq.length
+        |> should equal 6
---
+++ HexagonLib/CondorcetTournament.fs
@@ -0,0 +1,28 @@
+﻿module Hexagon.CondorcetTournament
+
+open Hexagon.Domain
+open Hexagon.Ais
+
+type Player = AiDescription * (AiCell [] -> TransactionParameters option) 
+
+type Game = {
+    Players: Player seq
+}
+
+let drawTournament (players: Player list) = 
+    if players |> Seq.length = 0 then List.empty<Game>
+    else 
+        let nbPlayers = players |> Seq.length
+        let lastPlayerId = players |> Seq.rev |> Seq.head |> fst |> fun p -> p.Id
+        let basicAis = 
+            [nbPlayers + 1..6]
+            |> List.map (fun i -> { Id = i + lastPlayerId; Name = sprintf "Basic AI %i" i }, Hexagon.BasicAi.play)
+        let players = players @ basicAis
+        let random = new System.Random()
+        let rand = fun () -> random.Next()
+        let randomizedPlayers = 
+            players
+            |> Seq.map (fun p -> p, rand())
+            |> Seq.sortBy snd
+            |> Seq.map fst
+        [{ Players = randomizedPlayers }]
