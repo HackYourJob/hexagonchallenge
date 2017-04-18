7c07d0f5ca9540ed7e07d61b21649b7f0a5b4bce
ouarzy <epecoul@gmail.com>
2017-04-18 11:01:13 +0200

    [RestApi] initialize Suave API with simple ok

---
+++ HexagonRestApi/App.fs
@@ -0,0 +1,9 @@
+﻿//inspired from http://blog.tamizhvendan.in/blog/2015/06/11/building-rest-api-in-fsharp-using-suave/
+
+open Suave.Web
+open Suave.Successful
+
+[<EntryPoint>]
+let main argv =
+  startWebServer defaultConfig (OK "Hello, Suave!")
+  0
