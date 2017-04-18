acf290f22731d64db4f62a16bbe460b79cc81638
ouarzy <epecoul@gmail.com>
2017-04-18 11:52:39 +0200

    [RestApi] implement generic get on resourceName

--- HexagonRestApi/RestFul.fs
+++ HexagonRestApi/RestFul.fs
@@ -1,2 +1,28 @@
-﻿module RestFul
+﻿namespace HexagonRestApi.Rest
+[<AutoOpen>]
+module RestFul =
+  open Newtonsoft.Json
+  open Newtonsoft.Json.Serialization
+  open Suave.Successful
+  open Suave
+  open Suave.Operators 
+  open Suave.Filters
+  open Suave.Successful
+    
+  type RestResource<'a> = {
+    GetAll : unit -> 'a seq
+  }
+
+
+  let JSON objectToSerialize =
+    let jsonSerializerSettings = new JsonSerializerSettings()
+    jsonSerializerSettings.ContractResolver <- new CamelCasePropertyNamesContractResolver()
+    JsonConvert.SerializeObject(objectToSerialize, jsonSerializerSettings) 
+    |> OK
+    >=> Writers.setMimeType("application/json; charset=utf-8")
+
+  let rest resourceName resource =
+    let resourcePath = "/" + resourceName
+    let getAll = warbler (fun _ -> resource.GetAll () |> JSON)
+    path resourcePath >=> GET >=> getAll
 
