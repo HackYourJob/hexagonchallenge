7c972ba1c6e731b2334f97f627cbf93f67a0b157
fpellet <git@florentpellet.com>
2017-04-18 23:03:32 +0200

    Check result type of play

--- HexagonLib/Compilator.Js.fs
+++ HexagonLib/Compilator.Js.fs
@@ -3,5 +3,15 @@
 open Hexagon
 open Fable.Core
 
-[<Emit("eval($0)()")>]
+[<Emit("""let func = eval($0)();
+return cells => {
+    let result = func(cells);
+    if(result == null || result == undefined) return undefined;
+
+    if(result.FromId === undefined) throw "Missing FromId";
+    if(result.ToId === undefined) throw "Missing FromId";
+    if(result.AmountToTransfer === undefined) throw "Missing FromId";
+
+    return { FromId: "" + result.FromId, ToId: "" + result.ToId, AmountToTransfer: + result.AmountToTransfer };
+}""")>]
 let compile (code: string): (Ais.AiCell[] -> Ais.TransactionParameters option) = jsNative
