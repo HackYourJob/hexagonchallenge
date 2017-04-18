102ca10e68221c8b2cbc167d6fbefc5d2fbe9310
fpellet <git@florentpellet.com>
2017-04-19 01:18:14 +0200

    pause on space and right array to play next step

--- HexagonLib/UI.Simulator.fs
+++ HexagonLib/UI.Simulator.fs
@@ -31,7 +31,7 @@ let runNextStep () =
             isRunning <- false
             isPaused <- false
 
-let rec play () =
+let rec autoPlay () =
     match isRunning, isPaused with
     | false, _ -> 
         nextStep <- End (Stopped, [])
@@ -39,12 +39,39 @@ let rec play () =
         ()
     | true, false -> 
         runNextStep()
-        setTimeout 10 play
+        setTimeout 10 autoPlay
 
 let startGame hexagonSize roundsNb ais = 
     nextStep <- Hexagon.Game.startGame handleMessage hexagonSize roundsNb ais
 
-    play()
+    autoPlay()
+
+let play basicAiJs =
+    match isPaused with
+    | true -> 
+        isPaused <- false
+        autoPlay()
+    | false -> 
+        isRunning <- true
+        isPaused <- false
+        
+        [
+            ({ Id = 1; Name = "Basic JS" }, basicAiJs )
+            ({ Id = 2; Name = "Basic F#" }, Hexagon.BasicAi.play )
+            ({ Id = 3; Name = "Dynamic JS" }, getPlayFunction() )
+        ]
+        |> startGame 9 5000
+
+let pause () =
+    match isRunning with
+    | false -> ()
+    | true -> 
+        isPaused <- not isPaused
+        autoPlay()
+
+let stop () =
+    isRunning <- false
+    isPaused <- false
 
 let addListenerOnClick (button: HTMLElement) action =
     button.addEventListener_click(fun _ -> 
@@ -57,31 +84,18 @@ let initializeAiSimulator basicAiJs =
     Board.initialize (document.getElementById("board"))
 
     let testButton = document.getElementById("test");
-    addListenerOnClick testButton (fun _ -> 
-        match isPaused with
-        | true -> 
-            isPaused <- false
-            play()
-        | false -> 
-            isRunning <- true
-            isPaused <- false
-        
-            [
-                ({ Id = 1; Name = "Basic JS" }, basicAiJs )
-                ({ Id = 2; Name = "Basic F#" }, Hexagon.BasicAi.play )
-                ({ Id = 3; Name = "Dynamic JS" }, getPlayFunction() )
-            ]
-            |> startGame 9 5000)
+    addListenerOnClick testButton (fun _ -> play basicAiJs)
         
     let stopButton = document.getElementById("stop");
-    addListenerOnClick stopButton (fun _ -> 
-        isRunning <- false
-        isPaused <- false)
+    addListenerOnClick stopButton stop
         
     let pauseButton = document.getElementById("pause");
-    addListenerOnClick pauseButton (fun _ -> 
-        match isRunning with
-        | false -> ()
-        | true -> 
-            isPaused <- not isPaused
-            play())
+    addListenerOnClick pauseButton pause
+
+    document.addEventListener_keydown (fun evt -> 
+        match evt.keyCode with
+        | 32.0 -> pause()
+        | 39.0 -> runNextStep()
+        | _ -> ()
+
+        new obj())
