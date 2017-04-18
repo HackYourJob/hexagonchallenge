ae57bbd148d253cf5e06ed10c46e71187e4b3c37
ouarzy <epecoul@gmail.com>
2017-04-18 12:00:03 +0200

    [RestApi] add dictionnary ai storage

--- HexagonRestApi/DB.fs
+++ HexagonRestApi/DB.fs
@@ -1,2 +1,12 @@
-﻿module Db
+﻿namespace SuaveRestApi.Db
+open System.Collections.Generic
+type Ai = {
+  AiName : string
+  UserName : string
+  Password : string
+}
+module Db =
+  let private aiStorage = new Dictionary<string, Ai>()
+  let getAis () =
+    aiStorage.Values |> Seq.map (fun ai -> ai)
 
