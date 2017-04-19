680173da5d73598797e1fdbe7879f35f06965ded
ouarzy <epecoul@gmail.com>
2017-04-19 18:29:07 +0200

    [Api] send data through post request (failed du to cross domaine??)

--- HexagonRestApi/AisService.fs
+++ HexagonRestApi/AisService.fs
@@ -3,22 +3,21 @@
 module AisService =
   open HexagonRestApi.Domain.Domain
  
-  let private buildAiId userId password aiName =
-    String.concat "." [userId; password; aiName;]
-  
+   let private buildAiId (ai: Ai)=
+    String.concat "." [ai.UserId; ai.Password; ai.AiName;]
+ 
   let getAis fromStorage =
     fromStorage.GetAll
 
   let submitAi intoStorage ai =
-    let id = buildAiId ai.UserId ai.Password ai.AiName
+    let id = buildAiId ai
     if intoStorage.Exists(id) then
         intoStorage.Update(id,ai)
     else
         intoStorage.Add(id,ai)        
 
-  let getAi fromStorage aiInfo=
-    let userId, password, aiName = aiInfo
-    let id = buildAiId userId password aiName
+  let getAi fromStorage ai=
+    let id = buildAiId ai
     if fromStorage.Exists id then
       Some (fromStorage.GetById id)
     else
