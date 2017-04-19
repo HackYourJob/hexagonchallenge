27dc18b7884d23e50ea3505f9d1632d4854466a4
ouarzy <epecoul@gmail.com>
2017-04-19 19:09:39 +0200

    [Api] hash pqw on erceived

--- HexagonRestApi/AisService.fs
+++ HexagonRestApi/AisService.fs
@@ -2,22 +2,29 @@
 
 module AisService =
   open HexagonRestApi.Domain.Domain
+  open BCrypt.Net
  
-   let private buildAiId (ai: Ai)=
+  let aiToAiWithEncrytedPwd ai =    
+    let hashedPwd = BCrypt.HashPassword(ai.Password, "$2a$12$VoOS3a0HBmjN67IoRtXV9e");
+    {AiName=ai.AiName;UserId=ai.UserId;Password=hashedPwd;Content=ai.Content}
+
+  let private buildAiId (ai: Ai)=
     String.concat "." [ai.UserId; ai.Password; ai.AiName;]
  
   let getAis fromStorage =
     fromStorage.GetAll
 
   let submitAi intoStorage ai =
-    let id = buildAiId ai
+    let aiWithEncryptedPwd = aiToAiWithEncrytedPwd ai
+    let id = buildAiId aiWithEncryptedPwd
     if intoStorage.Exists(id) then
-        intoStorage.Update(id,ai)
+        intoStorage.Update(id,aiWithEncryptedPwd)
     else
-        intoStorage.Add(id,ai)        
+        intoStorage.Add(id,aiWithEncryptedPwd)        
 
   let getAi fromStorage ai=
-    let id = buildAiId ai
+    let aiWithEncryptedPwd = aiToAiWithEncrytedPwd ai
+    let id = buildAiId aiWithEncryptedPwd
     if fromStorage.Exists id then
       Some (fromStorage.GetById id)
     else
