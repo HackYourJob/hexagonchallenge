2979ec4fb3bb343d08bb2603b6406581194c85de
fpellet <git@florentpellet.com>
2017-04-20 01:02:39 +0200

    Change webpack config to run in nodejs

--- tournamentWorker.webpack.config.js
+++ tournamentWorker.webpack.config.js
@@ -1,9 +1,20 @@
 var path = require("path");
 var webpack = require("webpack");
+var fs = require("fs");
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+    .filter(function (x) {
+        return ['.bin'].indexOf(x) === -1;
+    })
+    .forEach(function (mod) {
+        nodeModules[mod] = 'commonjs ' + mod;
+    });
 
 var cfg = {
     devtool: "source-map",
     entry: "./runTournamentWorker.js",
+    target: 'node',
     output: {
         path: path.join(__dirname, "build"),
         filename: "runTournamentWorker.js"
@@ -16,6 +27,9 @@ var cfg = {
               enforce: "pre"
           }
         ]
+    },
+    node: {
+        fs: 'empty'
     }
 };
 
