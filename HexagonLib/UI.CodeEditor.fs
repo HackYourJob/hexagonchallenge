14349cdf2105e0c68d08ff2a87558e249833dfe5
fpellet <florent.pellet@gmail.com>
2017-04-20 12:57:54 +0200

    Improve sample

--- HexagonLib/UI.CodeEditor.fs
+++ HexagonLib/UI.CodeEditor.fs
@@ -9,26 +9,24 @@ let initialize (container: HTMLElement) =
     editor <- Fable.Import.Globals.ace.edit(container)
     editor.getSession().setMode("ace/mode/javascript");
     editor.setValue("""(function(_) {
-return function(cells) {
-    // cells is an array of cell you own { Id : 'some-id', Resources : 12, Neighbours : [...] }, where 
-    // - Resources cannot be more than 100, it is the available resources to move from a cell
-    // - Neighbours is an array of cell, with Id and Resources properties only
-    // NOTE: you don't know who is the cell's owner, and by the way, Neighbours include your cells also
+    // you can store state here
+
+return function play(cells) {
+    // For help, you can click on "?" button, top right. (input/output details and rules)
     
     // Your clever code here :)
     let maxResourcesDiff = 0;
-        let selectedTuple;
-        cells.forEach(function (c) {
-            c.Neighbours.forEach(function (n) {
-                let resourcesDiff = c.Resources - n.Resources;
-                if (n.Owner !== "Own" && resourcesDiff > maxResourcesDiff) {
-                    maxResourcesDiff = resourcesDiff;
-                    selectedTuple = [c, n];
-                }
-            });
+    let selectedTuple;
+    cells.forEach(function (c) {
+        c.Neighbours.forEach(function (n) {
+            let resourcesDiff = c.Resources - n.Resources;
+            if (n.Owner !== "Own" && resourcesDiff > maxResourcesDiff) {
+                maxResourcesDiff = resourcesDiff;
+                selectedTuple = [c, n];
+            }
         });
-        return { FromId: selectedTuple[0].Id, ToId: selectedTuple[1].Id, AmountToTransfer: selectedTuple[0].Resources };
-    };
+    });
+    return { FromId: selectedTuple[0].Id, ToId: selectedTuple[1].Id, AmountToTransfer: selectedTuple[0].Resources };
 };
 })""") |> ignore
 
