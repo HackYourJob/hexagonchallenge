d130b403963c30e7a318d6533870342f05c96bfa
fpellet <florent.pellet@gmail.com>
2017-04-20 10:58:02 +0200

    Improve sample

--- HexagonLib/UI.CodeEditor.fs
+++ HexagonLib/UI.CodeEditor.fs
@@ -10,21 +10,25 @@ let initialize (container: HTMLElement) =
     editor.getSession().setMode("ace/mode/javascript");
     editor.setValue("""(function(_) {
 return function(cells) {
-    // cells if an array of cell you own { Id : 'some-id', Resources : 12, Neighbours : [...] }, where 
+    // cells is an array of cell you own { Id : 'some-id', Resources : 12, Neighbours : [...] }, where 
     // - Resources cannot be more than 100, it is the available resources to move from a cell
     // - Neighbours is an array of cell, with Id and Resources properties only
     // NOTE: you don't know who is the cell's owner, and by the way, Neighbours include your cells also
     
     // Your clever code here :)
-    
-    if (cells[cells.length - 1].Resources > 10 
-        && cells[cells.length - 1].Resources > cells[0].Neighbours[0].Resources) {
-        return { 
-            FromId: cells[cells.length - 1].Id, 
-            ToId: cells[0].Neighbours[0].Id, 
-            AmountToTransfer: cells[cells.length - 1].Resources - 1 
-        };
-    }
+    let maxResourcesDiff = 0;
+        let selectedTuple;
+        cells.forEach(function (c) {
+            c.Neighbours.forEach(function (n) {
+                let resourcesDiff = c.Resources - n.Resources;
+                if (n.Owner !== "Own" && resourcesDiff > maxResourcesDiff) {
+                    maxResourcesDiff = resourcesDiff;
+                    selectedTuple = [c, n];
+                }
+            });
+        });
+        return { FromId: selectedTuple[0].Id, ToId: selectedTuple[1].Id, AmountToTransfer: selectedTuple[0].Resources };
+    };
 };
 })""") |> ignore
 
